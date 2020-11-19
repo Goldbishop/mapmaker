f7c96107262ce709cd1a2d994109b161eaa236ac
John <3235535+Goldbishop@users.noreply.github.com>
2020-11-18 19:38:03 -0600

    Wrapped Application Load logic with Try-Catch

--- src/MapMaker.UI/src/edu/jeffstate/MapMaker.java
+++ src/MapMaker.UI/src/edu/jeffstate/MapMaker.java
@@ -4,11 +4,6 @@ import java.util.EventObject;
 
 import javafx.application.Application;
 
-import javafx.application.Platform;
-
-import javafx.event.ActionEvent;
-
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 import javafx.scene.Parent;
@@ -28,17 +23,22 @@ public class MapMaker extends Application {
     /*** JavaFX 11 implementation ***/
     @Override
     public void start(Stage stage) throws Exception {
-        //Parent root = FXMLLoader.load(getClass().getResource("MapMakerGUI.fxml"));
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("MapMakerGUI.fxml"));
-        Parent root = loader.load();
-        MapMakerGUIController controller = loader.getController();
-        controller.setStage(stage);
-        
-        var scene = new Scene(root);
+        try {
+            //Parent root = FXMLLoader.load(getClass().getResource("MapMakerGUI.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("MapMakerGUI.fxml"));
+            Parent root = loader.load();
+            MapMakerController controller = loader.getController();
+            controller.setStage(stage);
 
-        stage.setTitle("Map Maker");
-        stage.setScene(scene);
-        stage.show();
+            var scene = new Scene(root);
+
+            stage.setTitle("Map Maker");
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
