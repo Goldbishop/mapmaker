1270a5e49afaca58e8b77bd248ee00e54a0bb81c
John <3235535+Goldbishop@users.noreply.github.com>
2020-11-21 08:47:52 -0600

    Converted to INode to Generic

--- src/MapMaker.UI/src/edu/jeffstate/objects/INode.java
+++ src/MapMaker.UI/src/edu/jeffstate/objects/INode.java
@@ -5,11 +5,11 @@ import java.awt.Point;
 import java.util.Map;
 import java.util.Set;
 
-public interface INode {
+public interface INode<T> {
     /***
      * @return the collection of Exits
      */
-    public Map<DirectionEnum, INode> getExits();
+    public Map<DirectionEnum, T> getExits();
 
     /** 
      * @returns the Nodes's position on the map
