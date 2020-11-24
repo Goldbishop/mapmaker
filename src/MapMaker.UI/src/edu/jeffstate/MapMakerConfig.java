c08c066379ac81e233e376c9350de1fdafec5316
John <3235535+Goldbishop@users.noreply.github.com>
2020-11-24 09:20:30 -0600

    Added MapMakerConfig class

---
+++ src/MapMaker.UI/src/edu/jeffstate/MapMakerConfig.java
@@ -0,0 +1,27 @@
+package edu.jeffstate;
+
+import java.io.Serializable;
+
+public final class MapMakerConfig implements Serializable {
+    @SuppressWarnings("compatibility:-7856004910485516322")
+    private static final long serialVersionUID = 1L;
+
+    public MapMakerConfig() {
+        super();
+    }
+
+    }
+
+    /*** File Operations ***/
+    protected boolean ExportToFile() {
+        var result = false;
+
+        return result;
+    }
+
+    protected boolean ImportFromFile() {
+        var result = false;
+
+        return result;
+    }
+}
